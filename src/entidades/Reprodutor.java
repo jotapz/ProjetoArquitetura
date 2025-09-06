7684a2de387a5e85f53a50208b3150dfcd0663e0
LaraSLGomes <larastephanny05@gmail.com>
2025-09-06 17:46:37 -0300

    reprodutor

--- src/entidades/Reprodutor.java
+++ src/entidades/Reprodutor.java
@@ -1,4 +1,30 @@
 package entidades;
 
+import java.util.List;
+import interfaces.Tocavel;
+
 public class Reprodutor {
+
+    public void tocarItem(Tocavel item) {
+        System.out.println("▶️ Iniciando reprodução...");
+        List<Musica> musicasParaTocar = item.obterMusicas();
+        for (Musica musica : musicasParaTocar) {
+            System.out.println("Tocando agora: " + musica);
+         
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("⏹️ Fim da reprodução.");
+    }
+
+    public void pausar() {
+        System.out.println("⏸️ Reprodução pausada.");
+    }
+
+    public void parar() {
+        System.out.println("⏹️ Reprodução parada.");
+    }
 }

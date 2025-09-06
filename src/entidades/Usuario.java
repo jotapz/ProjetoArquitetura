dffb5010c59d5eae90fd999fa18a96221fd8a55e
stufflyxd <livros.pirt@gmail.com>
2025-09-06 16:20:06 -0300

    Esqueleto construido do projeto

---
+++ src/entidades/Usuario.java
@@ -0,0 +1,33 @@
+package entidades;
+
+
+import java.util.List;
+import java.util.UUID;
+
+public class Usuario {
+    private String id;
+    private String nome;
+    private String email;
+
+    public Usuario(String nome, String senha) {
+        this.id = UUID.randomUUID().toString();
+        this.nome = nome;
+        this.email = email;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNome(String nome) { this.nome = nome; }
+    public void setEmail(String email) { this.email = email; }
+}
